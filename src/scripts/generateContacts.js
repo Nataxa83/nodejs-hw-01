2ccbf7540a3aded56d77f018df86729fb5a62ac6
Nataliia Hordiienko <Natalihordienko@gmail.com>
2024-12-09 00:30:53 +0100

    done all

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,23 @@
-const generateContacts = async (number) => {};
+
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+
+const generateContacts = async (number) => {
+
+    const contactsList = await readContacts();
+    const newContacts = Array(number).fill(0).map(createFakeContact);
+    const newList = [...contactsList, ...newContacts];
+
+    await writeContacts(newList);
+    console.log(newList);
+
+    // fs.readFile(PATH_DB, (error, data) => {
+    //     console.log(data);
+    //     console.log(error);
+    // });
+    // console.log(contactsList);
+};
 
 generateContacts(5);
